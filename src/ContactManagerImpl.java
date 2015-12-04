868e8bc586dc62517ce22fbd02a32ae21a623d2f
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-12-04 03:49:28 +0000

    Implementation of Contact Manager, methods just have return no body.

---
+++ src/ContactManagerImpl.java
@@ -0,0 +1,74 @@
+import java.util.Calendar;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * @see ContactManager
+ */
+public class ContactManagerImpl implements ContactManager{
+
+    @Override
+    public int addFutureMeeting(Set<Contact> contacts, Calendar date) {
+        return 0;
+    }
+
+    @Override
+    public PastMeeting getPastMeeting(int id) {
+        return null;
+    }
+
+    @Override
+    public FutureMeeting getFutureMeeting(int id) {
+        return null;
+    }
+
+    @Override
+    public Meeting getMeeting(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Meeting> getFutureMeetingList(Contact contact) {
+        return null;
+    }
+
+    @Override
+    public List<Meeting> getFutureMeetingList(Calendar date) {
+        return null;
+    }
+
+    @Override
+    public List<PastMeeting> getPastMeetingList(Contact contact) {
+        return null;
+    }
+
+    @Override
+    public void addNewPastMeeting(Set<Contact> contacts, Calendar date, String text) {
+
+    }
+
+    @Override
+    public void addMeetingNotes(int id, String text) {
+
+    }
+
+    @Override
+    public void addNewContact(String name, String notes) {
+
+    }
+
+    @Override
+    public Set<Contact> getContacts(int... ids) {
+        return null;
+    }
+
+    @Override
+    public Set<Contact> getContacts(String name) {
+        return null;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+}
