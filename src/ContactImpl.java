f7034fd95f62dcc833a429756945f2964db7761e
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-12-04 02:48:05 +0000

    Added body to getters/setter, also added toString override.

--- src/ContactImpl.java
+++ src/ContactImpl.java
@@ -1,23 +1,61 @@
 import java.io.Serializable;
 
 public class ContactImpl implements Contact, Serializable {
+    private int id;
+    private String name;
+    private String notes;
+
+    /**
+     * Constructor for only id and name of contact,
+     *
+     * @param id of contact
+     * @param name of contact
+     */
+    public ContactImpl(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    /**
+     * Constructor for only id and name of contact,
+     *
+     * @param id of contact
+     * @param name of contact
+     */
+    public ContactImpl(int id, String name, String note){
+        new ContactImpl(id,name);
+        this.notes = note;
+    }
 
     @Override
     public int getId() {
-        return 0;
+        return this.id;
     }
 
     @Override
     public String getName() {
-        return "name";
+        return this.name;
     }
 
     @Override
     public String getNotes() {
-        return "notes";
+        return this.notes;
     }
 
     @Override
     public void addNotes(String note) {
+        this.notes += "\n" + note;
+    }
+
+    /**
+     * This method will return the contacts full details
+     * @return
+     */
+    @Override
+    public String toString(){
+        if(this.getNotes() == null) {
+            return "ID: " + this.getId() + "\nName: " + this.getName();
+        }
+        return "ID: " + this.getId() + "\nName: " + this.getName() + "\nNotes: " + this.getNotes();
     }
 }
