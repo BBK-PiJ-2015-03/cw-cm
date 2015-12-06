0c98d713fe62ba39375552530f4025ff46b30436
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-12-06 07:13:50 +0000

    Added methods with empty bodies and dummy return values for test purposes

---
+++ src/MeetingImpl.java
@@ -0,0 +1,22 @@
+import java.util.Calendar;
+import java.util.Set;
+
+/**
+ * @see Meeting
+ */
+public class MeetingImpl implements Meeting {
+    @Override
+    public int getId() {
+        return 0;
+    }
+
+    @Override
+    public Calendar getDate() {
+        return null;
+    }
+
+    @Override
+    public Set<Contact> getContacts() {
+        return null;
+    }
+}
