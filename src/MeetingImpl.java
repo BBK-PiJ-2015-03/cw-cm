1beeb8f74b5367ecf34a88971a4c5b798f1302a9
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-12-06 07:42:03 +0000

    Added constructor to help with testing

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -5,6 +5,22 @@ import java.util.Set;
  * @see Meeting
  */
 public class MeetingImpl implements Meeting {
+    private int meetingId;
+    private Calendar date;
+    private Set<Contact> meetingContacts;
+
+    /**
+     * Added this constructor for testing purposes, might keep if it will be useful.
+     * @param set of contacts that will be participating in the meeting
+     * @param meeting ID of the meeting
+     * @param date of the meeting
+     */
+    public MeetingImpl(Set<Contact> newContacts, int id, Calendar date) {
+        this.meetingContacts =  newContacts;
+        this.meetingId = this.hashCode();
+        this.date = date;
+    }
+
     @Override
     public int getId() {
         return 0;
@@ -12,11 +28,11 @@ public class MeetingImpl implements Meeting {
 
     @Override
     public Calendar getDate() {
-        return null;
+        return date;
     }
 
     @Override
     public Set<Contact> getContacts() {
-        return null;
+        return meetingContacts;
     }
 }
