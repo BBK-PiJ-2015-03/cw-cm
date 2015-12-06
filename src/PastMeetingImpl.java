690c0de959f7ec6010d4e877b3201f9a001400e7
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-12-06 22:52:36 +0000

    Added notes field

--- src/PastMeetingImpl.java
+++ src/PastMeetingImpl.java
@@ -1,15 +1,13 @@
-import java.util.Calendar;
-import java.util.Set;
-
 /**
  * @see PastMeeting & Meeting
  */
 public class PastMeetingImpl extends MeetingImpl implements PastMeeting {
+    private String notes = "";
     /**
      * @see PastMeeting#getNotes();
      */
     @Override
     public String getNotes() {
-        return ((this).getNotes());
+        return this.notes;
     }
 }
