bad41c3532d34f25749f9393a382d8372159888a
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-12-06 22:53:51 +0000

    Got rid of some unused objects

--- src/PastMeetingImplTest.java
+++ src/PastMeetingImplTest.java
@@ -12,24 +12,10 @@ import static org.junit.Assert.*;
  */
 public class PastMeetingImplTest {
     private PastMeeting m;
-    private Set<Contact> cSet;
-    private Contact test1;
-    private Contact test2;
-    private Contact test3;
-    private Contact test4;
 
     @Before
     public void setUp(){
         m = new PastMeetingImpl();
-        cSet = new HashSet<>();
-        test1 = new ContactImpl("Jane Doe");
-        test2 = new ContactImpl("John Doe");
-        test3 = new ContactImpl("Mark Mane");
-        test4 = new ContactImpl("Charles Fitz");
-        cSet.add(test1);
-        cSet.add(test2);
-        cSet.add(test3);
-        cSet.add(test4);
     }
 
     @Test
