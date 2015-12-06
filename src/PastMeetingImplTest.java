eb5a0a5420c5b5156a8878e97d556f27cfa4d655
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-12-06 23:00:41 +0000

    Tested extended methods + getNotes, all work fine, will double check later.

--- src/PastMeetingImplTest.java
+++ src/PastMeetingImplTest.java
@@ -11,15 +11,41 @@ import static org.junit.Assert.*;
  * Tests for PastMeeting.
  */
 public class PastMeetingImplTest {
-    private PastMeeting m;
+    private PastMeetingImpl m;
+    private Set<Contact> cSet;
+    private Contact test1;
+    private Contact test2;
 
     @Before
     public void setUp(){
         m = new PastMeetingImpl();
+        cSet = new HashSet<>();
+        test1 = new ContactImpl("Jane Doe");
+        test2 = new ContactImpl("John Doe");
+        cSet.add(test1);
+        cSet.add(test2);
+        m.setContacts(cSet);
+        m.setDate(Calendar.getInstance());
+        m.setId(m.hashCode());
     }
 
     @Test
     public void testGetNotes(){
         assertEquals("", m.getNotes());
+    }
+
+    @Test
+    public void testGetId() {
+        assertEquals(m.hashCode(), m.getId());
+    }
+
+    @Test
+    public void testGetDate() {
+        assertEquals(Calendar.getInstance(), m.getDate());
+    }
+
+    @Test
+    public void testGetContacts() {
+        assertEquals(cSet, m.getContacts());
     }
 }
