2bcbfb2503114c242cc494c638cce17b00d50ab9
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-12-06 07:41:48 +0000

    Trying to test, abit of trouble with constructor method.

--- src/MeetingImplTest.java
+++ src/MeetingImplTest.java
@@ -1,6 +1,9 @@
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Calendar;
+import java.util.HashSet;
+import java.util.Set;
 import static org.junit.Assert.*;
 
 /**
@@ -8,23 +11,46 @@ import static org.junit.Assert.*;
  */
 public class MeetingImplTest {
     private Meeting m;
+    private Set<Contact> cSet;
+    private Contact test1;
+    private Contact test2;
+    private Contact test3;
+    private Contact test4;
+    private Contact test5;
+    private Contact test6;
+    private Contact test7;
+    private Contact test8;
+    private Contact test9;
+    private Contact test10;
+
     @Before
     public void setUp(){
-        m = new MeetingImpl();
+        cSet = new HashSet<>();
+        cSet.add(test1);
+        cSet.add(test2);
+        cSet.add(test3);
+        cSet.add(test4);
+        cSet.add(test5);
+        cSet.add(test6);
+        cSet.add(test7);
+        cSet.add(test8);
+        cSet.add(test9);
+        m = new MeetingImpl(cSet, m.hashCode(), Calendar.getInstance());
+
     }
 
     @Test
     public void testGetId() {
-        assertEquals(0, 0);
+        assertEquals(m.hashCode(), m.getId());
     }
 
     @Test
     public void testGetDate() {
-        assertNull(m.getDate());
+        assertEquals(Calendar.getInstance(), m.getDate());
     }
 
     @Test
     public void testGetContacts() {
-        assertNull(m.getContacts());
+        assertEquals(cSet, m.getContacts());
     }
 }
