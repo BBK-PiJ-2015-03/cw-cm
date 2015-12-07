98420bed5fc52e123468f93a4380b2c4a671d582
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-12-07 02:05:39 +0000

    Adjusted casting as I was getting ClassCastException (or something similar)

--- src/ContactManagerImpl.java
+++ src/ContactManagerImpl.java
@@ -1,4 +1,5 @@
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -7,6 +8,7 @@ import java.util.Set;
  */
 public class ContactManagerImpl implements ContactManager{
     private Calendar todayDate = Calendar.getInstance();
+    private Set<Meeting> allMeetings = new HashSet<>();
     /**
      * @see ContactManager#addFutureMeeting(Set, Calendar)
      */
@@ -20,26 +22,47 @@ public class ContactManagerImpl implements ContactManager{
                 throw new IllegalArgumentException("Unknown Contact");
             }
         });
-        MeetingImpl newFutureMeeting = new FutureMeetingImpl();
-        newFutureMeeting.setContacts(contacts);
-        newFutureMeeting.setDate(date);
-        newFutureMeeting.setId(newFutureMeeting.hashCode());
-        return newFutureMeeting.getId();
+        Meeting meeting = new FutureMeetingImpl();
+        ((FutureMeetingImpl)meeting).setContacts(contacts);
+        ((FutureMeetingImpl)meeting).setDate(date);
+        ((FutureMeetingImpl)meeting).setId(meeting.hashCode());
+        allMeetings.add(meeting);
+        return meeting.getId();
     }
     /**
-     * Returns the PAST meeting with the requested ID, or null if it there is none.
+     * @see ContactManager#getPastMeeting(int)
+     */
+    @Override
+    public PastMeeting getPastMeeting(int id) throws IllegalArgumentException{
+        for (Meeting m : allMeetings) {  //Double check this loop
+            System.out.println(m.getId());
+            if(m.getId() == id){
+                if(m.getDate().after(todayDate)){
+                    throw new IllegalArgumentException("The date of this meeting is in the future.");
+                }
+                return ((PastMeetingImpl)m);
+            }
+        }
+        return null;
+    }
+    /**
+     * Returns the FUTURE meeting with the requested ID, or null if there is none.
      *
      * @param id the ID for the meeting
      * @return the meeting with the requested ID, or null if it there is none.
-     * @throws IllegalArgumentException if there is a meeting with that ID happening in the future
+     * @throws IllegalArgumentException if there is a meeting with that ID happening in the past
      */
     @Override
-    public PastMeeting getPastMeeting(int id) {
-        return null;
-    }
-
-    @Override
-    public FutureMeeting getFutureMeeting(int id) {
+    public FutureMeeting getFutureMeeting(int id) throws IllegalArgumentException{
+        for (Meeting m : allMeetings) {  //Double check this loop
+            System.out.println(m.getId());
+            if(m.getId() == id){
+                if(m.getDate().before(todayDate)){
+                    throw new IllegalArgumentException("The date of this meeting is in the past.");
+                }
+                return ((FutureMeetingImpl)m);
+            }
+        }
         return null;
     }
 
