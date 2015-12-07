506009ca965d12407dce79d0cbd05a474310266f
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-12-07 03:16:20 +0000

    Have changed meeting id from a hashcode to a count

--- src/MeetingImpl.java
+++ src/MeetingImpl.java
@@ -5,16 +5,30 @@ import java.util.Set;
  * @see Meeting
  */
 public class MeetingImpl implements Meeting {
-    private int Id;
+    private final int Id;
     private Calendar date;
     private Set<Contact> contacts;
+    private static int MEETING_COUNT = 0;
 
+
+    public MeetingImpl(Set<Contact> c, Calendar d){
+        this.date = d;
+        this.contacts = c;
+        this.Id = MEETING_COUNT+1;
+        MEETING_COUNT++;
+    }
+
+    public MeetingImpl(){
+        super();
+        this.Id = MEETING_COUNT+1;
+        MEETING_COUNT++;
+    }
 
     //Added setters for testing purposes, if useful will keep them.
     //This has solved the testing issue I was having, not sure why will ask Sergio.
 
     public void setId(int meetingId) {
-        this.Id = meetingId;
+        //this.Id = meetingId;
     }
 
     public void setDate(Calendar date) {
@@ -38,5 +52,10 @@ public class MeetingImpl implements Meeting {
     @Override
     public Set<Contact> getContacts() {
         return contacts;
+    }
+
+    @Override
+    public String toString(){
+        return "Meeting ID: " + this.getId();
     }
 }
