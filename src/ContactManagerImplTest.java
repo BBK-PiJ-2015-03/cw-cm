7225d32cbc73a9b8e0e932ac25a1b7a48f14070c
BBK-PiJ-2015-03 <sales@zozeri.com>
2015-12-07 04:31:17 +0000

    Using TDD to build up on the getFutureMeetingsList

--- src/ContactManagerImplTest.java
+++ src/ContactManagerImplTest.java
@@ -4,10 +4,7 @@ import org.junit.Test;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.GregorianCalendar;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import static org.junit.Assert.*;
 
@@ -17,6 +14,7 @@ import static org.junit.Assert.*;
 public class ContactManagerImplTest {
     private ContactManager cm;
     private Calendar futureDate;
+    private Calendar futureDate2;
     private Calendar pastDate;
     private Contact contact1;
     private Contact contact2;
@@ -27,6 +25,7 @@ public class ContactManagerImplTest {
         cm = new ContactManagerImpl();
         pastDate = new GregorianCalendar(1992,3,19); //For some reason any month entered is incremented by 1.
         futureDate = new GregorianCalendar(2019,11,6); //Maybe this is treated like an array where January is '0' etc...
+        futureDate2 = new GregorianCalendar(2017,2,12); //Maybe this is treated like an array where January is '0' etc...
         contact1 = new ContactImpl("Jane Doe");        //*** This has somehow fixed itself - not sure how or why.
         contact2 = new ContactImpl("Mark Pole");
         cSet = new HashSet<>();
@@ -57,24 +56,24 @@ public class ContactManagerImplTest {
 
     @Test
     public void testAddFutureMeeting() {
-        assertEquals(0, 0);
+        assertNotNull(cm.addFutureMeeting(cSet,futureDate));
     }
 
     @Test
     public void testGetPastMeeting() {  //Unless I create a constructor and add the meeting manually I cannot test this.
-        assertNull(cm.getPastMeeting(2)); //Will come back to this later
+       // assertNull(cm.getPastMeeting(2)); //Will come back to this later
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testGetPastMeetingWithFutureDate() {  //Unless I create a constructor and add the meeting manually I cannot test this.
-        int meetingId = cm.addFutureMeeting(cSet,futureDate);
-        assertNull(cm.getPastMeeting(meetingId)); //Will come back to this later
+    public void testGetPastMeetingWithFutureDate() {
+        int meetingId = cm.addFutureMeeting(cSet, futureDate);
+        assertNull(cm.getPastMeeting(meetingId));
     }
 
     @Test
-    public void testGetFutureMeetingListByDate() {
+    public void testGetFutureMeetingListByFutureDate() {
         cm.addFutureMeeting(cSet,futureDate);
-        assertNull(cm.getFutureMeetingList(futureDate));
+        assertTrue(cm.getFutureMeetingList(futureDate).size() == 1);
     }
 
     @Test
@@ -89,13 +88,16 @@ public class ContactManagerImplTest {
     }
 
     @Test
-    public void testGetFutureMeetingList() {
-        assertNull(cm.getFutureMeetingList(pastDate));
+    public void testGetFutureMeetingListByPastDate() {
+        List<Meeting> tempList = new ArrayList<>();
+        assertEquals(tempList, cm.getFutureMeetingList(pastDate));
     }
 
     @Test
-    public void testGetFutureMeetingList1() {
-        assertNull(cm.getFutureMeetingList(contact1));
+    public void testGetFutureMeetingListByContact() {
+        cm.addFutureMeeting(cSet, futureDate);
+        cm.addFutureMeeting(cSet, futureDate2);
+        assertNotNull(cm.getFutureMeetingList(contact1));
     }
 
     @Test
